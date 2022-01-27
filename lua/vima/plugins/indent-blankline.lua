4f3d305fec91a0af5c859eb319661e50b9c729eb
Ali 🚶 <ali.aminfar@gmail.com>
2022-01-27 16:56:46 +1100

    Fix some errors on indent line

--- lua/vima/plugins/indent-blankline.lua
+++ lua/vima/plugins/indent-blankline.lua
@@ -8,11 +8,30 @@ vim.opt.termguicolors = true
 vim.cmd([[highlight! link IndentBlanklineIndent1 NonText]])
 
 indent_blankline.setup({
-  char = '┊',
+  indentLine_enabled = 1,
+  char = '▏',
+  context_char = '▏',
+  filetype_exclude = {
+    'help',
+    'terminal',
+    'alpha',
+    'dashboard',
+    'packer',
+    'lspinfo',
+    'TelescopePrompt',
+    'TelescopeResults',
+    'nvchad_cheatsheet',
+    'lsp-installer',
+    '',
+  },
+  buftype_exclude = { 'terminal' },
+  show_trailing_blankline_indent = false,
+  show_first_indent_level = false,
   char_highlight_list = {
     'IndentBlanklineIndent1',
   },
+  use_treesitter = true,
   show_current_context = true,
   show_current_context_start = true,
-  filetype_exclude = { 'alpha', 'NvimTree' },
+  show_current_context_start_on_current_line = false,
 })
